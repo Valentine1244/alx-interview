6e42750cd8e6b06bc8724b116d0e261b08c2cc26
Akamigbo <akamigboifeanyi22@gmail.com>
2024-01-17 06:19:10 -0800

    operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Minimum operations """
+
+
+def minOperations(n):
+    """
+    A method that calculates the fewest
+    number of operations needed to result
+    in exactly n H characters in the file.
+
+    Parameters:
+    n (int): Number of H required in file
+
+    Returns:
+        Minimum number of steps
+    """
+    if n < 0:
+        return 0
+    if n % 1 > 0:
+        return 0
+    i = 2
+    if n == 1:
+        return 0
+    while (n % i > 0):
+        i += 1
+    if i < n:
+        ret = minOperations(n/i) + i
+    else:
+        ret = n
+    return int(ret)
